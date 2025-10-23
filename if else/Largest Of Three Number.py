b8c7697e7070a2049cada51da01e4fd0284aa6c8
Abhira <abhijeetsahu7978@gmail.com>
2025-10-24 00:08:42 +0530

    It Is To find The Greatest of Three number Enter

---
+++ if else/Largest Of Three Number.py
@@ -0,0 +1,10 @@
+a=int(input("Enter The First Number"))
+b=int(input("Enter The Second Number"))
+c=int(input("Enter The Third Number"))
+
+if(a>b and a>c):
+    print("First Nuber Is greater",a)
+elif(b>c):
+    print("Second number Is Greater",b)
+else:
+    print("Third Number Is Greater",c)
