6bb07007bf7be5fb8951ca50384a9f4436a55671
Abhira <abhijeetsahu7978@gmail.com>
2025-10-23 23:55:50 +0530

    It Is To Find The Grade Of the Student

---
+++ if else/Grade.py
@@ -0,0 +1,10 @@
+int=int(input("Enter The Grade"))
+
+if(int>=90):
+    print("It Was A Grade")
+elif(int>=80):
+    print("It Was Grade B")
+elif(int>=70):
+    print("It Was Grade C")
+else:
+    print("It Was A Grade D")
