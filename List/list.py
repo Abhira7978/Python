cb743883bebf21163f4d93004906f09eb486f435
Abhira <abhijeetsahu7978@gmail.com>
2025-10-27 23:21:49 +0530

    It Is To Add the Movie name in a list

---
+++ List/list.py
@@ -0,0 +1,3 @@
+list=[1,3,2]
+list.insert(1,5)
+print(list)
---
+++ List/movielist.py
@@ -0,0 +1,21 @@
+#it is the indirect method:1
+# Movie=[]
+# mov1=input("Enter The Name Of Mov1")
+# mov2=input("Enter The Name Of Mov2")
+# mov3=input("Enter The Name Of Mov3")
+
+# Movie.append(mov1)
+# Movie.append(mov2)
+# Movie.append(mov3)
+
+# print(Movie)
+
+
+
+movie=[]
+
+mov=movie.append(input("Enter The Name Of Mov1"))
+mov=movie.append(input("Enter The Name Of Mov2"))
+mov=movie.append(input("Enter The Name Of Mov3"))
+
+print(movie)
