212f906c5444a96da405d668e9d23bcb8e03d927
Abhira <abhijeetsahu7978@gmail.com>
2025-10-27 23:43:10 +0530

    It is The bothe question 1st is to count the grade and 2nd is to sort

---
+++ List/Studentgrade.py
@@ -0,0 +1,14 @@
+#It Is To Count The STudent With Grade A
+# Student=("C","D","A","A","B","B","A")
+
+# count=Student.count("A")
+# print(count)
+
+
+
+
+
+Student=["C","D","A","A","B","B","A"]
+
+Student.sort()
+print(Student)
