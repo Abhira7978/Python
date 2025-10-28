0f9f14699c4e4e63f39ed5ade05985ea6dd3c3bc
Abhira <abhijeetsahu7978@gmail.com>
2025-10-29 00:09:41 +0530

    It Is To Store Marks Of the Subjects

---
+++ dictionary and sets/mark.py
@@ -0,0 +1,12 @@
+Phy=input("Enter The Mark Of physics")
+Chem=input("Enter The Mark Of Chem")
+Maths=input("Enter The Marks Of Maths")
+
+mark={}
+
+mark.update({"Physics":Phy})
+mark.update({"Chemistry":Chem})
+mark.update({"Maths":Maths})
+
+print(type(mark))
+
